0e688f33c18191e12135a2c6b6ea29aaba887b67
Grishko <Agressor000000@gmail.com>
2019-12-22 19:31:06 +0200

    modified HW6

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,26 +1,22 @@
-let a = +prompt('Enter the first value:');
-let b = +prompt('Enter the second value:');
-let c = +prompt('Enter the third value:');
+let a = prompt('Enter the first value:');
+let b = prompt('Enter the second value:');
+let c = prompt('Enter the third value:');
 
-let x;
-let x1;
-let x2;
-let four = 4;
-let two = 2;
-let discriminant = b*b - four*a*c;
-let sqrDiscr = Math.sqrt(discriminant);
+let x, x1, x2, discr, sqrDiscr;
 
-if (isNaN(a) || isNaN(b) || isNaN(c)) {
+if (isNaN(a) || isNaN(b) || isNaN(c) || !a || !b || !c || +a === 0) {
     console.log('Invalid input data');
-} else if (a === 0 || b === 0 || c === 0) {
-    console.log('Invalid input data');
-} else if (discriminant < 0) {
-    console.log('no solution');
-} else if (discriminant === 0) {
-    x = - b / (two * a);
-    console.log(`x = ${x}`);
-} else if (discriminant > 0) {
-    x1 = Math.round((- b + sqrDiscr) / (two * a));
-    x2 = Math.round((- b - sqrDiscr) / (two * a));
-    console.log(`x1 = ${x1} and x2 = ${x2}`);
+} else {
+    discr = b*b - 4*a*c;
+    sqrDiscr = Math.sqrt(discr);
+    if (discr < 0) {
+        console.log('no solution');
+    } else if (discr === 0) {
+        x = - b / (2 * a);
+        console.log(`x = ${Math.round(x)}`);
+    } else if (discr > 0) {
+        x1 = (- b + sqrDiscr) / (2 * a);
+        x2 = (- b - sqrDiscr) / (2 * a);
+        console.log(`x1 = ${Math.round(x1)} and x2 = ${Math.round(x2)}`);
+    }
 }
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,18 +1,18 @@
-let a = prompt('Enter the first value:');
-let b = prompt('Enter the second value:');
-let c = prompt('Enter the third value:');
+let a = +prompt('Enter the first value:');
+let b = +prompt('Enter the second value:');
+let c = +prompt('Enter the third value:');
 
 
-if (isNaN(a) || isNaN(b) || isNaN(c)) {
+if (isNaN(a) || isNaN(b) || isNaN(c) || !Number.isInteger(a) || !Number.isInteger(b) || !Number.isInteger(c)) {
     alert('input values should be ONLY numbers ');
-} else if (+a <= 0 || +b <= 0 || +c <= 0) {
+} else if (a <= 0 || b <= 0 || c <= 0) {
     alert('A triangle must have 3 sides with a positive definite length')
-} else if(!(+a + +b > +c && +a + +c > +b && +c + +b > +a)) {
+} else if(!(a + b > c && a + c > b && c + b > a)) {
     alert('Triangle doesn’t exist');
     console.log('Triangle doesn’t exist');
-  } else if (+a === +b && +a === +c && +b === +c) {
+  } else if (a === b && a === c && b === c) {
     console.log('Equilateral triangle');
-  } else if (+a === +b || +a === +c || +b === +c) {
+  } else if (a === b || a === c || b === c) {
     console.log('Isosceles triangle');
   } else {
       console.log('Scalene triangle')
