d043df2b2231777c2756e61ae4d721ceb884ef07
Grishko <Agressor000000@gmail.com>
2019-12-24 23:46:15 +0200

    remove warning no-magic-numbers

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -3,20 +3,22 @@ let b = prompt('Enter the second value:');
 let c = prompt('Enter the third value:');
 
 let x, x1, x2, discr, sqrDiscr;
+let two = 2;
+let four = 4;
 
 if (isNaN(a) || isNaN(b) || isNaN(c) || !a || !b || !c || +a === 0) {
     console.log('Invalid input data');
 } else {
-    discr = b*b - 4*a*c;
+    discr = b*b - four*a*c;
     sqrDiscr = Math.sqrt(discr);
     if (discr < 0) {
         console.log('no solution');
     } else if (discr === 0) {
-        x = - b / (2 * a);
+        x = - b / (two * a);
         console.log(`x = ${Math.round(x)}`);
     } else if (discr > 0) {
-        x1 = (- b + sqrDiscr) / (2 * a);
-        x2 = (- b - sqrDiscr) / (2 * a);
+        x1 = (- b + sqrDiscr) / (two * a);
+        x2 = (- b - sqrDiscr) / (two * a);
         console.log(`x1 = ${Math.round(x1)} and x2 = ${Math.round(x2)}`);
     }
 }
