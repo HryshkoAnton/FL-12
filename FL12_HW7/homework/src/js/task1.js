bfaf5546658d3801015f751a599df6391ca5360d
Grishko <Agressor000000@gmail.com>
2019-12-25 02:35:09 +0200

    update first task of HW7

--- FL12_HW7/homework/src/js/task1.js
+++ FL12_HW7/homework/src/js/task1.js
@@ -7,6 +7,11 @@ let database = {
     'admin@gmail.com': 'AdminPass'
 }
 
+let changePassword;
+let newPassword;
+let newPassword2;
+let newPasswordValidLen = 6;
+
 
 if (!userEmail) {
     //If the userEmail is an empty line or Esc 
@@ -23,6 +28,48 @@ if (!userEmail) {
     } else if (password !== database[userEmail]) {
         //if visitor entered wrong password
         alert('Wrong password');
+    } else {
+        //if password is correct
+        //Suggest user/admin to change his password 
+        changePassword = confirm('Do you want to change your password?');
+
+        if (changePassword) {
+            // If user clicked ‘Ok’(change password)
+            password = prompt('Enter the old password');
+            if (!password) {
+                //If old password is an empty line or ‘Cancel’ 
+                alert('Canceled');
+            } else if (password !== database[userEmail]) {
+                // if visitor entered wrong old password
+                alert('Wrong password');
+            } else {
+                //If the visitor entered correct old password 
+                newPassword = prompt('Enter the new password');
+                if (!newPassword) {
+                    //If newPassword is an empty line or ‘Cancel’ 
+                    alert('Canceled');
+                } else if (newPassword.length < newPasswordValidLen) {
+                    //If newPassword length less than 6 
+                    alert('It’s too short password. Sorry')
+                } else {
+                    //if new password is valid
+                    newPassword2 = prompt('Enter the new password again');
+                    if (newPassword === newPassword2) {
+                        //If user write the same new password
+                        alert('You have successfully changed your password.');
+                        database[userEmail] = newPassword;
+                    } else {
+                        //If the inputted value doesn’t match the new password
+                        alert('You wrote the wrong password.');
+                    }
+                }
+            }
+        } else {
+            //if change password has canceled
+            alert('You have failed the change.');
+        }
+
+        console.log(database);
     }
 } else {
     //If the visitor has entered an email address that is not in the database
@@ -30,46 +77,3 @@ if (!userEmail) {
 }
 
 
-//Suggest user/admin to change his password 
-let changePassword = confirm('Do you want to change your password?')
-let newPassword;
-let newPassword2;
-let newPasswordValidLen = 6;
-
-if (changePassword) {
-    // If user clicked ‘Ok’(change password)
-    password = prompt('Enter the old password');
-    if (!password) {
-        //If old password is an empty line or ‘Cancel’ 
-        alert('Canceled');
-    } else if (password !== database[userEmail]) {
-        // if visitor entered wrong old password
-        alert('Wrong password');
-    } else {
-        //If the visitor entered correct old password 
-        newPassword = prompt('Enter the new password');
-        if (!newPassword) {
-            //If newPassword is an empty line or ‘Cancel’ 
-            alert('Canceled');
-        } else if (newPassword.length < newPasswordValidLen) {
-            //If newPassword length less than 6 
-            alert('It’s too short password. Sorry')
-        } else {
-            //if new password is valid
-            newPassword2 = prompt('Enter the new password again');
-            if (newPassword === newPassword2) {
-                //If user write the same new password
-                alert('You have successfully changed your password.');
-                database[userEmail] = newPassword;
-            } else {
-                //If the inputted value doesn’t match the new password
-                alert('You wrote the wrong password.');
-            }
-        }
-    }
-} else {
-    //if change password has canceled
-    alert('You have failed the change.');
-}
-
-console.log(database);
