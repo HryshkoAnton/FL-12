07df3f39a6f6b67a59bee7d8050ea7e8e81dfb89
Grishko <Agressor000000@gmail.com>
2019-12-30 02:34:43 +0200

    add isLeapYear func

--- FL12_HW8/homework/js/isLeapYear.js
+++ FL12_HW8/homework/js/isLeapYear.js
@@ -1,3 +1,15 @@
-function isLeapYear() {
-    
+function isLeapYear(date) {
+    let year = new Date(date);
+    if (!isNaN(year)) {
+        year = year.getFullYear();
+        if ( ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0) ) {
+            return `${year} is a leap year`;
+        } else {
+            return `${year} is not a leap year`;
+        }
+    } else {
+        return year;
+    }
 }
+
+isLeapYear('2020-01-01 00:00:00');
