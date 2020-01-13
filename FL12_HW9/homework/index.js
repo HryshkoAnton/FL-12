99471055346b04d612fbbbcc9f2ff108ef7e565b
Grishko <Agressor000000@gmail.com>
2020-01-13 08:14:40 +0200

    change some func to func expressions, reuse mapArray func, add variables

--- FL12_HW9/homework/index.js
+++ FL12_HW9/homework/index.js
@@ -1,39 +1,32 @@
-// let convert = (...args) => [...args].map(x => typeof x === 'string' ? parseInt(x) : x.toString());
-function convert(...args) {
-    let result = [];
-    for (let arg of args) {
-      if (typeof arg === 'string') {
-        result.push(parseInt(arg));
-      } else {
-        result.push(arg.toString());
-      }
-    }
-  return result;
-}
-convert('1', 2, 3, '4'); // [1, '2', '3', 4]
+const a = 2;
+const b = 3;
+const c = 8;
+const d = 7;
+const e = 48;
+const f = 31;
+const year = 2019;
+const g = 365;
+
+let convert = (...args) => mapArray(args, (x) => typeof x === 'string' ? parseInt(x) : x.toString());
+convert('1', a, b, '4'); // [1, '2', '3', 4]
 
 
-// let executeforEach = (arr, func) => arr.forEach(x => func(x));
 function executeforEach(arr, func) {
     for (let i of arr) {
       func(i);
     }
 }
-executeforEach([1,2,3], (el) => console.log(el * 2)); // logs 2 4 6
+executeforEach([1,a,b], (el) => console.log(el * a)); // logs 2 4 6
 
 
-// let mapArray = (arr, func) => arr.map(x => func(parseInt(x)));
 function mapArray(arr, func) {
-    let result = [];
-    for (let i of arr) {
-      result.push(func(parseInt(i)));
-    }
-    return result;
+  let result = [];
+  executeforEach(arr, (elem) => result.push(func(elem)));
+  return result;
 }
-mapArray([2, '5', 8], (el) => el + 3); // returns [5, 8, 11]
+mapArray([a, '5', c], (el) => parseInt(el) + b); // returns [5, 8, 11]
 
 
-// let filterArray = (arr, func) => arr.filter(func);
 function filterArray(arr, func) {
     let result = [];
     executeforEach(arr, element => {
@@ -43,10 +36,9 @@ function filterArray(arr, func) {
     });
     return result;
 }
-filterArray([2, 5, 8], (el) => el % 2 === 0); // returns [2, 8]
+filterArray([a, d, c], (el) => el % a === 0); // returns [2, 8]
 
 
-// let flipOver = str => str.split('').reverse().join('');
 function flipOver(str) {
     let result = '';
     for (let i=1; i<str.length+1; i++) {
@@ -64,30 +56,21 @@ function makeListFromRange(arr) {
     }
     return result;
 }
-makeListFromRange([2, 7]); // [2, 3, 4, 5, 6, 7]
+makeListFromRange([a, d]); // [2, 3, 4, 5, 6, 7]
 
 
 const actors = [
     { name: 'tommy', age: 36 },
     { name: 'lee', age: 28 }
 ];
-  
-// let getArrayOfKeys = (arr, key) => arr.map(x => x[key]);
-function getArrayOfKeys(arr, key) {
-    let result = [];
-    executeforEach(arr, obj => result.push(obj[key]));
-    return result;
-}
+
+let getArrayOfKeys = (arr, key) => mapArray(arr, (obj) => obj[key]);
 getArrayOfKeys(actors, 'name'); // ['tommy', 'lee']
 
 
 const minValue = 30;
-let substitute = arr => {
-  return mapArray(arr, elem => {
-    return elem < minValue ? '*' : elem;
-  })
-}
-substitute([58, 14, 48, 2, 31, 29]); // [58, '*', 48, '*', 31, '*']
+let substitute = arr => mapArray(arr, (elem) => elem < minValue ? '*' : elem);
+substitute([c, e, a, f, d]); // ['*', 48, '*', 31, '*']
 
 
 function getPastDay(date, days) {
@@ -96,7 +79,23 @@ function getPastDay(date, days) {
     return subtractedDate.getDate();
 }
   
-const date = new Date(2019, 0, 2);
+const date = new Date(year, 0, a);
 console.log(getPastDay(date, 1)); // 1, (1 Jan 2019)
-console.log(getPastDay(date, 2)); // 31, (31 Dec 2018)
-console.log(getPastDay(date, 365)); // 2, (2 Jan 2018)
+console.log(getPastDay(date, a)); // 31, (31 Dec 2018)
+console.log(getPastDay(date, g)); // 2, (2 Jan 2018)
+
+function formatDate(date) {
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const days = date.getDate();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const twoDigits = 10;
+  return (
+    `${year}/${month + 1}/${days} ` +
+    `${hours < twoDigits ? '0' : ''}${hours}:` +
+    `${minutes < twoDigits ? '0' : ''}${minutes}`
+  );
+}
+console.log(formatDate(new Date('6/15/2018 09:15:00')));
+console.log(formatDate(new Date()));
